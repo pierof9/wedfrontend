b345dd07f3e4b4886ac7aebeeccd2542918e4cf2
pierof <piero.farina092@gmail.com>
2025-01-05 18:11:47 +0100

    changin port to backend service

--- src/pages/RSVP.jsx
+++ src/pages/RSVP.jsx
@@ -26,7 +26,7 @@ const RSVP = () => {
     e.preventDefault();
 
     try {
-      const response = await axios.post("http://localhost:3001/submit", formData);
+      const response = await axios.post("https://wedbackend-production.up.railway.app/submit", formData);
       navigate("/"); // Replace "/" with your main page route
     } catch (error) {
       console.error("Failed to submit form:", error);
