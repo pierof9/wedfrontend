38c3859139571825967df70641dfb19e4760f9b3
pierof <piero.farina092@gmail.com>
2025-01-05 17:40:52 +0100

    2nd commit

---
+++ tailwind.config.cjs
@@ -0,0 +1,12 @@
+module.exports = {
+  content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        cream: "#fdfaf4", // Light cream background
+        rose: "#e5989b", // Soft rose color
+      },
+    },
+  },
+  plugins: [],
+};
